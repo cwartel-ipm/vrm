6928a1013725242098525e2582a9538c04583e1e
cwartel-ipm <73844079+cwartel-ipm@users.noreply.github.com>
2024-03-05 16:55:08 +0000

    adapt to new pdf link 2

--- client/SPA.js
+++ client/SPA.js
@@ -88,7 +88,7 @@ function observePrintButton() {
       my_print(
         node
           .getAttribute("data-pdf")
-          .replaceAll("https://borne.irigo.fr/", "/"),
+          .replaceAll("https://borne.irigo.fr/", "/fh/"),
       );
     };
     observePrintButton();
