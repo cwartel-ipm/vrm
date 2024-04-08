634c774e68610e68aa2fb4e54397f9bf6251e73a
ced <cedric.wartel@protonmail.com>
2024-04-08 15:30:29 +0000

    accessibility positionning

--- client/SPA.js
+++ client/SPA.js
@@ -135,7 +135,7 @@ observeAndTweakNode("#is-Header-Logo", function (node) {
 document.addEventListener("DOMContentLoaded", (event) => {
 
   waitForElm(document,"#is-Features").then(function (node) {
-    node.style.top = "400px";
+    node.style.top = "700px";
   });
   
   waitForElm(document,"#is-Header-Logo-Link").then(function (node) {
